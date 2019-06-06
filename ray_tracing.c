1484be24ac491bb9cbb19066e8cb119514346985
hi15 <hi15@inf.ufpr.br>
2019-06-06 11:41:54 -0300

    End function trace

--- ray_tracing.c
+++ ray_tracing.c
@@ -2,7 +2,12 @@
 #include <stdbool.h>
 #include <limits.h>
 #include <math.h>
+
 #define MAX_RAY_DEPTH 3
+#define X 0
+#define Y 1
+#define Z 2
+#define max(a,b) (a > b) ? a : b
 
 typedef struct {
 	int direction[3];
@@ -22,24 +27,28 @@ void subtractXYZ(double *result, double *p1, double *p2){
 	result[2] = p1[2] - p2[2];
 }
 
+double dot(double *v1, double *v2){
+	return v1[0] * v2[0] + v1[1] * v2[1] + v1[2] * v2[2];
+}
 //functions of vec3
 void normalize(double *vec){
-	double nor2 = vec[0] * vec[0] + vec[1] * vec[1] + vec[2] * vec[2]; 
+	double nor2 = dot(vec,vec); 
 	if (nor2 > 0) {
 		double inv_nor = 1 / sqrt(nor2);
 		vec[0] *= inv_nor;
-		vec[2] *= inv_nor;
 		vec[1] *= inv_nor;
+		vec[2] *= inv_nor;
 	}
 }
+
 
 //functions of Sphere
 bool intersect(Object *obj, double *rayorig, double *raydir, double *t0, double *t1){
 	double l[3], tca, d2, thc;
 	subtractXYZ(l,obj->center,rayorig);
-	tca = raydir[0] * l[0] + raydir[1] * l[1] + raydir[2] * l[2]; //l.dot(raydir)
+	tca = dot(l,raydir); //l.dot(raydir)
 	if (tca < 0) return false;
-	d2 = (l[0] * l[0] + l[1] * l[1] + l[2] * l[2]) - (tca * tca) ; //l.dot(l)
+	d2 = dot(l,l)- (tca * tca) ; //l.dot(l)
 	if(d2 > obj->radius2) return false;
 	thc = sqrt(obj->radius2 - d2);
 	*t0 = tca - thc;
@@ -48,10 +57,112 @@ bool intersect(Object *obj, double *rayorig, double *raydir, double *t0, double 
 }
 
 
+double mix(double a, double b, double mx){
+	return b * mx + a * (1 - mx);
+} 
 
-int Trace(Ray *ray, int depth){
+/* This is the main trace function. It takes a ray as argument (defined by its origin and direction).Test if this ray intersects any of the geometry in the scene. 
+If the ray intersects an object, compute the intersection point, the normal at the intersection point, and shade this point using this information. 
+Shading depends on the surface property (is it transparent, reflective, diffuse). The function returns a color for the ray. 
+If the ray intersects an object that is the color of the object at the intersection point, otherwise it returns the background color. */
+double* Trace(double *ray_orig, double *ray_dir, int depth, Object *obj,int obj_size){
+	double tnear = INFINITY;
+	int i;
+	Object *object = NULL;
+	static double color[3];
+	// find intersection of this ray with the sphere in the scene
+	for (i=0; i < obj_size; i++) {
+		double t0 = INFINITY, t1 = INFINITY;
+		if (intersect(&obj[i],ray_orig,ray_dir,&t0,&t1)) {
+			if (t0 < 0) t0 = t1;
+			if (t0 < tnear) {
+				tnear = t0;
+				object = &obj[i];
+			}
+		}		
+	}
 
-	return 0;
+	// if there's no intersection return black or background color
+	if (!object) {
+		for (i=0; i<3; i++)
+			color[i] = 2;
+			return color;
+	}
+	double surfacecolor[3] = { 0.0, 0.0, 0.0 };
+	double phit[3], nhit[3];
+	for (i=0; i<3; i++)
+		phit[i] = ray_orig[i] + ray_dir[i] * tnear;
+	for (i=0; i<3; i++)
+		nhit[i] = phit[i] - object->center[i];
+	normalize(nhit); //normalize normal direction
+	// If the normal and the view direction are not opposite to each other reverse the normal direction. 
+	//That also means we are inside the sphere so set the inside bool to true. Finally reverse the sign of IdotN which we want positive.
+	double bias = 1e-4; //add some vias to the poin to be traced
+	bool inside = false;
+	if (dot(ray_dir,nhit) > 0) {
+		for (i=0; i<3; i++)
+			nhit[i] = -nhit[i];
+		inside = true;
+	}
+	double ray_orig2[3];
+	if ((object->transparency > 0 || object->reflection > 0) && depth < MAX_RAY_DEPTH) {
+		double facingratio = -dot(ray_dir,nhit);
+		//change the mix value to tweak the effect
+		double fresneleffect = mix(pow(1-facingratio,3),1,0.1);
+		//compute reflection direction
+		double refldir[3];
+		double dot_raydir_nhit = dot(ray_dir,nhit);
+		for(i=0; i<3; i++)
+			refldir[i]-=nhit[i]*2*dot_raydir_nhit;
+			ray_orig2[i] = phit[i] + nhit[i] * bias;
+		double * reflection = trace(ray_orig2, refldir, obj, depth+1);
+		double * refraction = NULL;
+		// if the sphere is also transparent compute refraction ray
+		if (object->transparency) {
+			double ior = 1.1, eta = (inside) ? ior : 1/ior; //inside the surface?
+			double cosi = -dot(nhit,ray_dir);
+			double k = 1 - eta*eta*(1 - cosi*cosi);
+			double refrdir[3], ksqrt = sqrt(k);
+			for(i=0; i<3; i++)
+				refrdir[i] = ray_dir[i]*eta + nhit[i] * (eta * cosi - ksqrt);
+				ray_orig2[i] = phit[i] + nhit[i] * bias;
+			normalize(refrdir);
+			refraction = trace(ray_orig2, refldir, obj, depth+1);
+		} 
+			double refraction_aux[3] = { 0.0, 0.0, 0.0 };
+			if(refraction == NULL) refraction = refraction_aux;
+			for(i=0; i<3; i++)
+				surfacecolor[i] = (reflection[i]*fresneleffect + refraction[i] * (1 - fresneleffect) * object->transparency) * object->surface_color;
+	} else {
+		//diffuse object
+		for (i=0; i < obj_size; i++){
+			if (obj->emission_color[X] > 0) {
+				//light
+				double transmission[3] = { 1.0, 1.0, 1.0 };
+				double light_direction[3];
+				for(int j=0; j<3; j++)
+					light_direction[j] = obj[i]->center[j] - phit[j];
+				normalize(light_direction);
+				for(int j=0; j < obj_size; j++){
+					if( i != j){
+						double t0, t1;
+						for(int k=0; k<3; k++)
+							ray_orig2[k] = phit[k] + nhit[k] * bias;
+						if(insersect(obj[i],light_direction, t0, t1)){
+							trasmission = 0;
+							break;
+						}
+					}
+				}
+				for(int j=0; j<3; j++)
+					surfacecolor[i] += object->surface_color[i] * trasmission[i] * max(0.0,dot(nhit,light_direction)) * object->emission_color[i];
+			}
+		}
+		
+	}
+	for(i=0; i<3; i++)
+		color[i] = surfacecolor[i] + object->emission_color[i];
+	return color;
 }
 
 
